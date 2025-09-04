187e56ded120b624ce64f400fc8311dbfff27bb6
Thom Koopman <11939567+Thokoop@users.noreply.github.com>
2025-09-04 21:07:05 +0200

    Unset Flask debug mode

--- webconfig/server.py
+++ webconfig/server.py
@@ -896,5 +896,6 @@ if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
         port=int(core_config.FLASK_PORT),
-        debug=True,
+        debug=False,
+        use_reloader=False,
     )
